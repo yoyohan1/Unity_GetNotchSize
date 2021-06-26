486697a74eec5cb2de95457b357cba0d1d3f5a59
yoyohan1 <836052290@qq.com>
2021-06-26 11:01:52 +0800

    2.0

--- Unity_GetNotchSize/Assets/GetNotchSize/Plugins/iOS/GetNotchSize.h
+++ Unity_GetNotchSize/Assets/GetNotchSize/Plugins/iOS/GetNotchSize.h
@@ -3,7 +3,7 @@
 /*
  * iPhone刘海屏工具类
  */
-@interface iOSUtil : NSObject
+@interface GetNotchSize : NSObject
  
 // 判断是否是刘海屏
 +(BOOL)getIsNotch;
@@ -13,14 +13,5 @@
 
 // 判断是否是横条Home
 +(BOOL)getIsHome;
- 
-// 获取横条Home高度
-+(CGFloat)getHomeSize;
-
-// 获取刘海屏高度
-+(NSString*)getiPhoneType;
-
-// 获取剩余空间
-+ (long long)getDiskFreeSize;
  
 @end
